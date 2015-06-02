3ecb5e8b41b1b1c190939afeb0a91da7ecb9283a
ZjhYmy <1239263775@qq.com>
2015-06-02 17:54:59 +0800

    首次提交
    首次提交

--- RedWine/AppDelegate.m
+++ RedWine/AppDelegate.m
@@ -7,7 +7,12 @@
 //
 
 #import "AppDelegate.h"
-
+#import "FindViewController.h"
+#import "FirstViewController.h"
+#import "ChatViewController.h"
+#import "CupboardViewController.h"
+#import "OurViewController.h"
+#import "LoginViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -15,31 +20,56 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+//    LoginViewController *login = [[LoginViewController alloc]init];
+//    self.window.rootViewController = login;
+    
+    
+    UITabBarController *tabBarController = [[UITabBarController alloc]init];
+    self.window.rootViewController = tabBarController;
+
+    
+    //首页
+    FirstViewController *firstVC = [[FirstViewController alloc]init];
+    firstVC.tabBarItem.title = @"首页";
+    firstVC.tabBarItem.image = [UIImage imageNamed:@"6.png"];
+    UINavigationController *firstNav = [[UINavigationController alloc]initWithRootViewController:firstVC];
+
+    
+    
+    //聊天
+    ChatViewController *chatVC = [[ChatViewController alloc]init];
+    chatVC.tabBarItem.title = @"聊天";
+    chatVC.tabBarItem.image = [UIImage imageNamed:@"2.png"];
+    UINavigationController *chatNav = [[UINavigationController alloc]initWithRootViewController:chatVC];
+    
+    //酒柜
+    CupboardViewController *cupboardVC = [[CupboardViewController alloc]init];
+    cupboardVC.tabBarItem.title = @"酒柜";
+    cupboardVC.tabBarItem.image = [UIImage imageNamed:@"4.png"];
+    UINavigationController *cupboardNav  = [[UINavigationController alloc]initWithRootViewController:cupboardVC];
+    
+    //发现
+    FindViewController *findVC = [[FindViewController alloc]init];
+    findVC.tabBarItem.title = @"发现";
+    findVC.tabBarItem.image = [UIImage imageNamed:@"1.png"];
+    UINavigationController *findNav = [[UINavigationController alloc]initWithRootViewController:findVC];
+
+    
+    //我的
+    OurViewController *ourVC = [[OurViewController alloc]init];
+    ourVC.tabBarItem.title = @"我的";
+    ourVC.tabBarItem.image = [UIImage imageNamed:@"3.png"];
+    UINavigationController *ourNav  = [[UINavigationController alloc]initWithRootViewController:ourVC];
+    
+    
+    tabBarController.viewControllers = @[firstNav,chatNav,cupboardNav,findNav,ourNav];
+    [self.window makeKeyAndVisible];
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
